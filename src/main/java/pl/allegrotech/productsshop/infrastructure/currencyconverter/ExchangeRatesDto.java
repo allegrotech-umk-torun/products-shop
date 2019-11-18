4c8ea6a3361a8f3060acad4f5c101829473b77ee
Marcin Radoszewski <marcin.radoszewski@allegro.pl>
2019-11-18 08:46:28 +0100

    Implemented the exchange client

---
+++ src/main/java/pl/allegrotech/productsshop/infrastructure/currencyconverter/ExchangeRatesDto.java
@@ -0,0 +1,63 @@
+package pl.allegrotech.productsshop.infrastructure.currencyconverter;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Objects;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class ExchangeRatesDto {
+    private final String base;
+    private final String date;
+    private final Map<String, BigDecimal> rates;
+
+    @JsonCreator
+    ExchangeRatesDto(
+            @JsonProperty("base") String base,
+            @JsonProperty("date") String date,
+            @JsonProperty("rates") Map<String, BigDecimal> rates) {
+        this.base = base;
+        this.date = date;
+        this.rates = rates;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    Map<String, BigDecimal> getRates() {
+        return rates;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ExchangeRatesDto that = (ExchangeRatesDto) o;
+        return Objects.equals(base, that.base) &&
+                Objects.equals(date, that.date) &&
+                Objects.equals(rates, that.rates);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(base, date, rates);
+    }
+
+    @Override
+    public String toString() {
+        return "ExchangeRatesDto{" +
+                "base='" + base + '\'' +
+                ", date='" + date + '\'' +
+                ", rates=" + rates +
+                '}';
+    }
+}
+
---
+++ src/main/java/pl/allegrotech/productsshop/infrastructure/currencyconverter/ExchangeRestApiClient.java
@@ -0,0 +1,24 @@
+package pl.allegrotech.productsshop.infrastructure.currencyconverter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+class ExchangeRestApiClient {
+    private final RestTemplate http;
+    private final String baseUrl;
+
+    ExchangeRestApiClient(RestTemplate exchangeRestTemplate,
+                          @Value("${client.exchange.url}") String baseUrl) {
+        this.http = exchangeRestTemplate;
+        this.baseUrl = baseUrl;
+    }
+
+    ExchangeRatesDto getExchangeRates(String base) {
+        String url = String.format("%s/latest?base=%s", baseUrl, base);
+        ResponseEntity<ExchangeRatesDto> response = http.getForEntity(url, ExchangeRatesDto.class);
+        return response.getBody();
+    }
+}
